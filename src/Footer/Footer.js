2721436a31b31bb4422d9d964dd9b73d3db3054b
KarapetyanAram <aramkarapetyan222@mail.ru>
2022-06-03 04:54:43 +0800

    post-project

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,7 +4,29 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer-cont">
-    <div> Footer</div>
+    <div> 
+      <h1>OUR PURPOSE</h1>
+    <p>w;/dfmllkflkwbfkjlqwbfkjlqwbfjlqkwebfklqwfbqbwflkjqbwk</p>
+    </div>
+    <div>
+     <h1> OUR STUDIO</h1>
+     <p> Mariam,Aram,Vigen</p>
+    </div>
+    <div>
+      <h1>OUR SOCIAL NETWORK</h1>
+      <a href="https://www.facebook.com/" target="_blank">
+            
+      <img src="https://pnggrid.com/wp-content/uploads/2021/07/Facebook-Logo-Square-768x768.png" className="imgFooter"></img>
+          </a>
+          <a href="https://www.instagram.com/" target="_blank">
+  
+          <img src="https://img.pngio.com/instagram-clipart-psd-instagram-logo-png-hd-download-transparent-instagram-logo-png-880_803.png" className="imgFooter"></img>
+          </a>
+          <a href="https://web.telegram.org/z/" target="_blank">
+  
+           <img src="https://image.similarpng.com/thumbnail/2021/01/Telegram-icon-on-transparent-background-PNG.png" className="imgFooter"></img>
+          </a>
+    </div>
     </div>
   );
 }
--- src/components/Postes/Postes.js
+++ src/components/Postes/Postes.js
@@ -8,7 +8,7 @@ import classes from "./Postes.module.css"
 const Postes = () => {
     const { postesData, setPostesData } = usePostes()
     const [aretext,setAretext] = useState({coment:''})
-    
+    const [comment, setComment] = useState("")
     const { user } = useUserInfo()
     const addPost=()=>{
        postesData.push(aretext)
@@ -20,7 +20,17 @@ const Postes = () => {
         setPostesData([...postesData])
     }
 
+    const addComment=(id)=>{
+        let post=postesData?.find((e)=>(e.id==id));
+        
+        post.comments.push(comment)
+        setPostesData([...postesData])
+        setComment('')
+    }
+
     
+
+    //console.log(postesData);
     useEffect(()=>{},[postesData])
     return (
            <div className={classes.newPost}>
@@ -30,7 +40,7 @@ const Postes = () => {
                 <input type="text"
                         value={aretext.coment}
                         onChange={(evt) =>{
-                        setAretext({...aretext,coment:evt.target.value,id:new Date().getTime(),title: "json-server",count:0,author: user,"picture": "https://media.cntraveller.com/photos/611bf0b8f6bd8f17556db5e4/1:1/w_2000,h_2000,c_limit/gettyimages-1146431497.jpg"})
+                        setAretext({...aretext,coment:evt.target.value,id:new Date().getTime(),title: "json-server",count:0,author: user,"picture": "https://media.cntraveller.com/photos/611bf0b8f6bd8f17556db5e4/1:1/w_2000,h_2000,c_limit/gettyimages-1146431497.jpg", comments: []})
                         }}
                     className={classes.newPostArea}
                 />
@@ -45,8 +55,21 @@ const Postes = () => {
                                 <p>{post.coment}</p>
                                 <p><img src={post.picture} className={classes.postPicture}/></p>
                                 <div className={classes.likes}>
-                                <img src="https://w7.pngwing.com/pngs/394/403/png-transparent-heart-shape-love-heart-love-heart-shape.png" className={classes.srtik} onClick={()=>{countPlus(post.id)}}/>
-                                <p>{post.count}</p>
+                                    <img src="https://w7.pngwing.com/pngs/394/403/png-transparent-heart-shape-love-heart-love-heart-shape.png" className={classes.srtik} onClick={()=>{countPlus(post.id)}}/>
+                                    <p className={classes.postLikes}>{post.count}</p>
+                                </div>
+                                <div className={classes.comments}>
+                                    <div> 
+                                        <input type="text" placeholder="Write your comment" value={comment} onChange={e => setComment(e.target.value)} />
+                                        <button onClick={() => addComment(post.id)}>write</button>
+                                    </div>
+                                    <ul className={classes.decComment}>
+                                        {post.comments.map((comment, index) => {
+                                            return (
+                                                <li key={index}>{comment} </li>
+                                            )
+                                        })}
+                                    </ul>
                                 </div>
                             </div>
                         )
